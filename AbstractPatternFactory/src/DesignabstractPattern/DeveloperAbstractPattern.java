a85246c126bb3db8fc9e1458f23f4bb08a3673a3
2223532 <2223532@LTIN395123.cts.com>
2023-10-30 17:50:37 +0530

    Initial Commit

---
+++ AbstractPatternFactory/src/DesignabstractPattern/DeveloperAbstractPattern.java
@@ -0,0 +1,11 @@
+package DesignabstractPattern;
+
+public class DeveloperAbstractPattern  extends AbstractFactory{
+
+	@Override
+	Developer getDeveloper(String typeOfDeveloper) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
